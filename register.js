f17dfb066e3d698b9596ac8be5c6945f1864b684
kamal sritha <kamalsritha@kamals-MacBook-Air.local>
2022-12-29 21:23:27 +0530

    First commit

---
+++ register.js
@@ -0,0 +1,10 @@
+const name=document.getElementById('name')
+const Mobile no.=document.getElementById('Mobile no.')
+const email=document.getElementById('email')
+const password=document.getElementById('password')
+const password-repeat=document.getElementById('password-repeat')
+const occupation=document.getElementById('occupation')
+const experience=document.getElementById('experience')
+const Available =document.getElementById('Available timmings')
+
+form
